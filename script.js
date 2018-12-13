1271a623be189df897144e7264a32c50a734acd9
Vanessa <44301380+sanguliupo@users.noreply.github.com>
2018-12-12 21:32:16 -0800

    optimize code

--- script.js
+++ script.js
@@ -3,17 +3,21 @@ var color1 = document.querySelector('.color1');
 var color2 = document.querySelector('.color2');
 var body = document.getElementById('gradient');
 
+var button = document.querySelector('button');
+
 function setGradient() {
 	body.style.background =
 		'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
 	css.textContent = body.style.background + ';';
 }
 
-color1.addEventListener('input', setGradient);
-
-color2.addEventListener('input', setGradient);
-
-var button = document.querySelector('button');
+function toHexString(number) {
+	var hexString = number.toString(16);
+	if (hexString.length % 2) {
+		hexString = '0' + hexString;
+	}
+	return hexString;
+}
 
 function randomColors() {
 	var Random1 = Math.round(Math.random() * 255);
@@ -40,27 +44,19 @@ function randomColors() {
 		'))';
 	css.textContent = body.style.background + ';';
 
-	function toHexString(number) {
-		var hexString = number.toString(16);
-		if (hexString.length % 2) {
-			hexString = '0' + hexString;
-		}
-		return hexString;
-	}
-
 	color1.value =
 		'#' +
 		toHexString(Random1) +
 		toHexString(Random2) +
-		toHexString(Random3) +
-		'';
+		toHexString(Random3);
 
 	color2.value =
 		'#' +
 		toHexString(Random4) +
 		toHexString(Random5) +
-		toHexString(Random6) +
-		'';
+		toHexString(Random6);
 }
 
+color1.addEventListener('input', setGradient);
+color2.addEventListener('input', setGradient);
 button.addEventListener('click', randomColors);
